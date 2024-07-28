461148a3d3d07680e9283f530319bd6c278eaca2
nxvtej <nxvtej@outlook.com>
2024-07-28 23:07:13 +0530

    set sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,33 @@
 /** @format */
 
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Mobilenav from "./Mobilenav";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+			<Link href='/' className='flex items-center gap-1'>
+				<Image
+					src={"/icons/logo.svg"}
+					width={32}
+					height={32}
+					alt='Connect Pro'
+					className='max-sm:size-10'
+				/>
+
+				<p className='text-[26px] font-extrabold text-white max-sm:hidden'>
+					Connect Pro``
+				</p>
+			</Link>
+
+			<div className='flex-between gap-5'>
+				{/* {} */}
+				<Mobilenav />
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
